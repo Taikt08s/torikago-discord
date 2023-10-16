77bd56ce55b2d3026da38e16494f6ecf8a4606cc
Taikt08s <styematic@gmail.com>
2023-10-16 19:05:09 +0700

    channel edit + delete channel

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -15,7 +15,7 @@ interface ServerMemberProps {
 const roleIconMap = {
     [MemberRole.GUEST]: null,
     [MemberRole.MODERATOR]: <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
-    [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />
+    [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 ml-2 text-yellow-500" />
 }
 
 export const ServerMember = ({
