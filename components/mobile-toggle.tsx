3807004ea2d9cb104840e3bbe5ced740e29d94e9
Taikt08s <styematic@gmail.com>
2023-10-17 23:57:28 +0700

    socket io set up

--- components/mobile-toggle.tsx
+++ components/mobile-toggle.tsx
@@ -1,13 +1,13 @@
 import { Menu } from "lucide-react"
+
 import {
     Sheet,
     SheetContent,
-    SheetTrigger
+    SheetTrigger,
 } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
 import { ServerSidebar } from "@/components/server/server-sidebar";
-
 
 export const MobileToggle = ({
     serverId
@@ -17,17 +17,16 @@ export const MobileToggle = ({
     return (
         <Sheet>
             <SheetTrigger asChild>
-                <Button variant={"ghost"} size={"icon"} className="md:hidden">
+                <Button variant="ghost" size="icon" className="md:hidden">
                     <Menu />
                 </Button>
             </SheetTrigger>
-            <SheetContent side={"left"} className="p-0 flex gap-0">
+            <SheetContent side="left" className="p-0 flex gap-0">
                 <div className="w-[72px]">
                     <NavigationSidebar />
                 </div>
                 <ServerSidebar serverId={serverId} />
             </SheetContent>
         </Sheet>
-
     )
 }
